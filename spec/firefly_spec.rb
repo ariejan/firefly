a813b4a413dab7e71eb36ab197d520a2db3cc969
Ariejan de Vroom <ariejan@ariejan.net>
2010-03-26 21:14:33 +0100

    Initial commit of the FireFly project

---
+++ spec/firefly_spec.rb
@@ -0,0 +1,16 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "FireFly" do
+  include Rack::Test::Methods
+
+  def app
+    @app ||= Sinatra::Application
+  end
+
+  it "should respond to /" do
+    get '/'
+    last_response.should be_ok
+  end
+end
