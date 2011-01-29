190516149f97aa75d74abc1580f7c3fc5cd2adfc
Ariejan de Vroom <ariejan@ariejan.net>
2011-01-29 15:25:37 +0100

    Split up specs, removed white space pollution

--- lib/firefly/url.rb
+++ lib/firefly/url.rb
@@ -10,12 +10,12 @@ module Firefly
     property :code,         String,     :index => true, :length => 64
     property :clicks,       Integer,    :default => 0
     property :created_at,   DateTime,   :default => Proc.new{Time.now}
-    
+
     # Increase the visits counter by 1
     def register_click!
       self.update(:clicks => self.clicks + 1)
     end
-    
+
     # Shorten a long_url and return a new FireFly::Url
     def self.shorten(long_url, code = nil)
       code = nil if code !~ /\S/
@@ -24,19 +24,19 @@ module Firefly
       return nil unless valid_code?(code)
 
       long_url = normalize_url(long_url)
-      
+
       the_url = Firefly::Url.first(:url => long_url) || Firefly::Url.create(:url => long_url)
       code ||= Firefly::Base62.encode(the_url.id.to_i)
       the_url.update(:code => code) if the_url.code.nil?
       the_url
-    end  
-    
+    end
+
     private
       # Normalize the URL
       def self.normalize_url(url)
         URI.parse(URI.escape(url)).normalize.to_s
       end
-      
+
       # Validates the URL to be a valid http or https one. 
       def self.valid_url?(url)
         url.match(Firefly::Url::VALID_URL_REGEX)
