aa7ae33513a22c85733fc39eff08f0c24f13632e
Ariejan de Vroom <ariejan@ariejan.net>
2012-05-29 20:38:30 +0200

    Fresh Rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firefly::Application.config.secret_token = 'e1a27c77ed2b50f19f46662e13dd4349e1e9ae26189dae374eb1a7afba9fe192e9eda9c75d0dfc41305d43ec08660d9199307938acb029f89bc8a7c5641c1703'
