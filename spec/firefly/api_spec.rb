c81846481bf1b6e496ec93c6ddbef77fe4fb887f
Ariejan de Vroom <ariejan@ariejan.net>
2012-11-11 17:21:51 +0100

    Refactory of spec_helper
    Load `app` properly instead of using class variables.

--- spec/firefly/api_spec.rb
+++ spec/firefly/api_spec.rb
@@ -1,12 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "API" do
-  include Rack::Test::Methods
-
-  def app
-    @@app
-  end
-
   [:post, :get].each do |verb|
     describe "adding a URL by #{verb.to_s.upcase}" do
       it "should be okay adding a new URL" do
