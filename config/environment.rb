2e0d4e04234ba76b107dff930ec0c3e323ac1798
Ariejan de Vroom <ariejan@ariejan.net>
2016-02-15 12:11:07 +0100

    Generate API app

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,10 @@ require 'rubygems'
 require 'bundler/setup'
 require 'hanami/setup'
 require_relative '../lib/firefly'
+require_relative '../apps/api/application'
 require_relative '../apps/web/application'
 
 Hanami::Container.configure do
+  mount Api::Application, at: '/api'
   mount Web::Application, at: '/'
 end
