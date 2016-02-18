10c49cafe2301f50aba55411b86023b91b26ee3e
Ariejan de Vroom <ariejan@ariejan.net>
2016-02-18 09:40:37 +0100

    Remove unused feature file

--- spec/web/features/url_redirect_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'features_helper'
-
-describe 'URL redirect' do
-  # This feature test is useless, because Capybara
-  # does not handle redirects to external domains.
-  # See spec/web/controller/items/resolver_spec.rb instead.
-end
