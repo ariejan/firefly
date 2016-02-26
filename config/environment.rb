7fb8b4f025138886fd6da93c7077261ea5e4194e
Ariejan de Vroom <ariejan@ariejan.net>
2016-02-26 13:53:09 +0100

    Generate admin app

--- config/environment.rb
+++ config/environment.rb
@@ -2,10 +2,12 @@ require 'rubygems'
 require 'bundler/setup'
 require 'hanami/setup'
 require_relative '../lib/firefly'
+require_relative '../apps/admin/application'
 require_relative '../apps/api/application'
 require_relative '../apps/web/application'
 
 Hanami::Container.configure do
+  mount Admin::Application, at: '/admin'
   mount Api::Application, at: '/api'
   mount Web::Application, at: '/'
 end
