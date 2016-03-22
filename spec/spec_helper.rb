14e11f9d4162b6145e515d66f5da99faad810ddc
Ariejan de Vroom <ariejan@ariejan.net>
2016-03-22 11:05:10 +0100

    Configure simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,14 @@
 # Require this file for unit tests
 ENV['HANAMI_ENV'] ||= 'test'
 
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter 'app/*/application.rb'
+end
 
 require_relative '../config/environment'
 require 'minitest/autorun'
