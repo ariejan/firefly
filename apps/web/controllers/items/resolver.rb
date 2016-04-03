28ea9bc2d2cc9c7722bca8ecdaab0a4e9814257c
Ariejan de Vroom <ariejan@ariejan.net>
2016-04-03 22:38:06 +0200

    Set resolver Cache-Control header
    Fixes #75

--- apps/web/controllers/items/resolver.rb
+++ apps/web/controllers/items/resolver.rb
@@ -2,6 +2,8 @@ module Web::Controllers::Items
   class Resolver
     include Web::Action
 
+    cache_control :private, max_age: 90
+
     def call(params)
       @item = ItemRepository.find_by_code(params[:code])
       result = ClickRepository.register_for(@item)
